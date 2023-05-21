d3afdbd6ba9886d231d14568ece1b8dc14eb0796
Sam Hernandez <samd.hernandezc24@gmail.com>
2023-05-20 18:53:39 -0600

    changes in the root file

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,24 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import {RemixBrowser} from '@remix-run/react'
+import {startTransition, StrictMode} from 'react'
+import {hydrateRoot} from 'react-dom/client'
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
-});
+function hydrate() {
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>
+    )
+  })
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate)
+} else {
+  console.log('wefef')
+  window.setTimeout(hydrate, 1)
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,47 @@
-import type {LinksFunction} from '@remix-run/node'
-import {Links, LiveReload, Outlet, Scripts} from '@remix-run/react'
+import type {LinksFunction, V2_MetaFunction} from '@remix-run/node'
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
 
 import stylesUrl from '~/styles/tailwind.css'
 
 export const links: LinksFunction = () => [{rel: 'stylesheet', href: stylesUrl}]
 
+export const meta: V2_MetaFunction = () => {
+  const title = 'BloodPlus: ¡Dona vida!'
+  const description =
+    'Dona vida a quien más lo necesite y siempre ten la oportunidad de sonreír con BloodPlus.'
+  const viewport = 'width=device-width,initial-scale=1'
+  return [
+    {title: title},
+    {
+      name: 'description',
+      content: description,
+    },
+    {
+      name: 'viewport',
+      content: viewport,
+    },
+  ]
+}
+
 export default function App() {
   return (
-    <html lang="en">
+    <html className="h-full" lang="en">
       <head>
+        {/** Todas las metaexportaciones de todas las rutas irán aquí */}
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>BloodPlus: ¡Dona vida!</title>
+        <Meta />
         <Links />
       </head>
       <body className="bg-wash font-text text-lg font-medium leading-base text-secondary antialiased">
         <Outlet />
+        <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
